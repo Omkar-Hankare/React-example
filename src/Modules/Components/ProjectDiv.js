a687d136854535669656370b417ee9653152e7f8
Omkar Hankare <omkar.hankare@maersk.com>
2022-01-22 22:05:06 +0530

    => Removed React starter files => Added Module Folder and added Components and Pages Subfolder => Created files & components  for StartingPage.js

---
+++ src/Modules/Components/ProjectDiv.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProjectDiv = () => {
+  return <div></div>;
+};
+
+export default ProjectDiv;
---
+++ src/Modules/Pages/StartingPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ProjectDiv from "../Components/ProjectDiv";
+const StartingPage = () => {
+  return <ProjectDiv></ProjectDiv>;
+};
+
+export default StartingPage;
